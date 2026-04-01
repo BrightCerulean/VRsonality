2be90e978d10b1a2e9320de5c144c1ce4c01b0c0
jmh160430 <jmh160430@utdallas.edu>
2026-04-01 09:31:27 -0500

    WIP Improved portal system
    PortalTrigger - Collider detection and chooses which teleport to use
    Portal - abstract class.
    RoomTeleport - Teleports to new room in same scene. Uses spawn points.
    SceneTeleport - Teleports to new scene. Uses ID of the spawn point in the new scene.
    SpawnPoint - Contains the ID of the spawnpoint. Create new invisible objects in the scene as spawn points and attach this to them.
    SpawnManager - Deals with SceneTeleport to find the spawnpoint with the same name in the new scene. Not done yet.
    
    To-do later: Add quick fade-to-black transition to hide jankiness.

--- Assets/Scripts/Scripts/SceneTeleport.cs
+++ Assets/Scripts/Scripts/SceneTeleport.cs
@@ -5,14 +5,11 @@ using UnityEngine.SceneManagement;
 public class SceneTeleport : Portal
 {
     public string sceneToLoad; // Set in Inspector e.g. "PastRoom"
-    public Transform targetSpawnPoint; // Set in Inspector to where Player should spawn after entering portal
+    public string targetSpawnID; // Name of the spawnpoint in the new scene where the player should spawn
 
     public override void Activate(GameObject player)
     {
+        SpawnManager.Instance.SetSpawnID(targetSpawnID);
         SceneManager.LoadScene(sceneToLoad);
-        //Set player position and rotation after scene transition
-        Rigidbody rb = player.GetComponent<Rigidbody>();
-        rb.position = targetSpawnPoint.position;
-        player.transform.rotation = targetSpawnPoint.rotation;
     }
 }
---
+++ Assets/Scripts/Scripts/SpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager Instance;
+    private string targetSpawnID; //Name of spawnpoint where the player should spawn
+
+    public void SetSpawnID(string id)
+    {
+        targetSpawnID = id;
+    }
+
+    //Finish doing the rest of this
+}
