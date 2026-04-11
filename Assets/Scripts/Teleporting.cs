552b687d0a756e8940278e52ab4938807bf1607c
Navaneeta Padmakumar <navaneeta.pkumar@gmail.com>
2026-04-11 14:47:09 -0500

    Save local scene and script changes

--- Assets/Scripts/Teleporting.cs
+++ Assets/Scripts/Teleporting.cs
@@ -3,21 +3,26 @@ using UnityEngine;
 public class Teleporting : MonoBehaviour
 {
     public Transform playerTransform;
-    public float playerHeight = 1.8f;
-    private string aButton = "js5";
+    public float playerHeight = 3f;
 
-    //public void TeleportTo(Vector3 point)
-    public void TeleportTo(Vector3 point, bool isInteractable)
-    {
-        // To distinguish between floor and object and teleport only if it is floor
-        if (isInteractable) return;
-        if (!Input.GetButtonDown(aButton) && !Input.GetKeyDown(KeyCode.T)) 
+    void Update()
+    {   
+        Debug.Log("[Teleporting] Ray origin: " + Camera.main.transform.position + " direction: " + Camera.main.transform.forward);
+        Debug.Log("[Teleporting] Checking for teleport input...");
+        Debug.Log("[Teleporting] T pressed: " + Input.GetKeyDown(KeyCode.T));
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton5))
         {
-            return;
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit[] hits = Physics.RaycastAll(ray, 50f);
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.CompareTag("Floor"))
+                {
+                    playerTransform.position = hit.point + Vector3.up * playerHeight;
+                    Debug.Log("[Teleporting] Teleported to: " + hit.point);
+                }
+            }
         }
-        playerTransform.position = point + Vector3.up * playerHeight;
-        Debug.Log("Teleported to : " + point);
-
     }
-
 }
