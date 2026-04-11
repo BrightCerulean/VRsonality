4891ad252f16d11661ce7c89280ce46f987731fd
Navaneeta Padmakumar <navaneeta.pkumar@gmail.com>
2026-04-11 16:04:37 -0500

    Teleportation  setup

--- Assets/Scripts/Teleporting.cs
+++ Assets/Scripts/Teleporting.cs
@@ -6,11 +6,8 @@ public class Teleporting : MonoBehaviour
     public float playerHeight = 3f;
 
     void Update()
-    {   
-        Debug.Log("[Teleporting] Ray origin: " + Camera.main.transform.position + " direction: " + Camera.main.transform.forward);
-        Debug.Log("[Teleporting] Checking for teleport input...");
-        Debug.Log("[Teleporting] T pressed: " + Input.GetKeyDown(KeyCode.T));
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton5))
+    {
+        if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.JoystickButton5))
         {
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
             RaycastHit[] hits = Physics.RaycastAll(ray, 50f);
@@ -21,6 +18,7 @@ public class Teleporting : MonoBehaviour
                 {
                     playerTransform.position = hit.point + Vector3.up * playerHeight;
                     Debug.Log("[Teleporting] Teleported to: " + hit.point);
+                    break;
                 }
             }
         }
