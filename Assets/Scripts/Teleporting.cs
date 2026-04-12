3d59540c7fa610bd3b0d2bcf217d290dd3f2c0cd
jmh160430 <jmh160430@utdallas.edu>
2026-04-11 19:20:44 -0500

    Added extra buttons

--- Assets/Scripts/Teleporting.cs
+++ Assets/Scripts/Teleporting.cs
@@ -4,10 +4,22 @@ public class Teleporting : MonoBehaviour
 {
     public Transform playerTransform;
     public float playerHeight = 3f;
+    public static string YButton;
 
+    private void Awake()
+    {
+        if (Application.isEditor)//PC
+        {
+            YButton = "js0";
+        }
+        else//Android
+        {
+            YButton = "js2";
+        }
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.JoystickButton5))
+        if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.JoystickButton5) || Input.GetButtonDown(YButton))
         {
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
             RaycastHit[] hits = Physics.RaycastAll(ray, 50f);
